7d4c694ffcdd7f37a82b4a87d2de6850138a4180
luxingwen <shuaicode@gmail.com>
2023-07-06 00:22:23 +0800

    first commit

---
+++ config/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@umijs/max';
+import proxy from './proxy';
+import routes from './routes';
+
+export default defineConfig({
+  favicons: ['/github-logo.jpg'],
+  esbuildMinifyIIFE: true,
+  codeSplitting: { jsStrategy: 'granularChunks' },
+  ignoreMomentLocale: true,
+  fastRefresh: true,
+  hash: true,
+  crossorigin: {},
+  deadCode: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: 'Sgin',
+  },
+  routes,
+  proxy,
+  npmClient: 'pnpm',
+  tailwindcss: {},
+  icons: {},
+  valtio: {},
+});
---
+++ config/proxy.ts
@@ -0,0 +1,8 @@
+export default {
+    '/api': {
+      target: 'http://localhost:8080/', //目标服务器地址
+      // target: 'https://testchat.kimways.com/',
+      changeOrigin: true,
+    },
+  };
+  
